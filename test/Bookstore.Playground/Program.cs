f68b4cb15c62840c2988298e8fffc67eb23ecf8e
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-07 14:44:30 +0200

    ComposableFilterBy

--- test/Bookstore.Playground/Program.cs
+++ test/Bookstore.Playground/Program.cs
@@ -11,44 +11,12 @@ using (var scope = LinqPadRhetosHost.CreateScope(rhetosHostAssemblyPath))
     var context = scope.Resolve<Common.ExecutionContext>();
     var repository = context.Repository;
 
-    var allBooks = repository.Bookstore.Book.Load().ToList();
-    allBooks.Dump(); //Nisam siguran kako ostatak napravit da mi se pokaze samo title i autor, te ime autora bez da koristim .Select
-
-
-    var query = repository.Bookstore.Book
-        .Query()
-        .Select(b => new { b.Title, b.Author.Name });
-
-    // ToList will force Entity Framework to load the data from the database.
-    var items = query.ToList();
-    items.Dump();
-
-    query.ToString().Dump();
-
-    Console.WriteLine("Unesite broj knjiga koliko zelite unjest:");
-    var NumberOfBooks = int.Parse(Console.ReadLine());
-
-    /*for (int i = 0; i < NumberOfBooks; i++)
+    var filterParameter = new Bookstore.LongBooks3
     {
-        Console.WriteLine($"Unesite naslov {i+1}. knjige: ");
-        var bookTitle = Console.ReadLine();
-
-        var newBook = new Bookstore.Book { Code = i+1.ToString(), Title = bookTitle };
-
-        repository.Bookstore.Book.Insert(newBook);
-    }*/
-
-    var actionParameter = new Bookstore.InsertBooks
-    {
-        NumberOfBooks = NumberOfBooks,
-        TitlePrefix = "A Song of Ice and Fire"
+        MinimumPages = 100,
+        ForeignBooksOnly = true
     };
-
-    repository.Bookstore.InsertBooks.Execute(actionParameter);
-
-    var filterParameter = new Bookstore.LongBooks();
-    var query1 = repository.Bookstore.Book.Query(filterParameter);
-    query1.ToString().Dump();
-    query1.ToSimple().ToList().Dump();
-    //scope.CommitAndClose();
+    var query = repository.Bookstore.Book.Query(filterParameter);
+    query.ToString().Dump(); // Print the SQL query.
+    query.ToSimple().ToList().Dump(); // Load and print the books.
 }
