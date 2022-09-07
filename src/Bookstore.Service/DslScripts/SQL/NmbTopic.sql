aff2e913d7cd9eb98ce2f67a38ae990e6ec49a4a
Mislav Žnidarec <mznidarec@omega-software.hr>
2022-09-07 10:25:44 +0200

    Day 2 - Read-only Data Structures

---
+++ src/Bookstore.Service/DslScripts/SQL/NmbTopic.sql
@@ -0,0 +1,9 @@
+﻿SELECT
+	b.ID,
+	NumberOfTopics = COUNT(t.ID)
+FROM
+	Bookstore.Book b
+	INNER JOIN Bookstore.BookTopic bt ON b.ID = bt.BookID
+	INNER JOIN Bookstore.Topic t ON bt.TopicID = t.ID
+GROUP BY
+	b.ID
